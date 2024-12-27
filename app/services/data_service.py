0ff7eeced5327c8c1015cda70e9538fb92c78476
pascmma <spartacus.crixus45@gmail.com>
2024-12-27 05:10:27 -0500

    v1.12

--- app/services/data_service.py
+++ app/services/data_service.py
@@ -21,15 +21,9 @@ def formato_tablas(tabla):
 
 def get_data_service(tabla):
     try:
-        print("INICIO DE GET")
-        # Obtener nombres de las columnas de la tabla
         columnas = [column.name for column in tabla.__table__.columns]
-
-        # Obtener todos los datos de la tabla
         datos = db.session.query(tabla).all()
-        print("DATOS: ",datos)
 
-        # Verificar si se han recuperado datos
         if not datos:
             raise Exception("No se encontraron datos en la tabla.")
 
@@ -38,23 +32,21 @@ def get_data_service(tabla):
         for fila in datos:
             fila_dict = {}
             for col in columnas:
-                fila_dict[col] = getattr(fila, col)
+                try:
+                    fila_dict[col] = getattr(fila, col)  
+                except AttributeError:
+                    fila_dict[col] = None  
             datos_dict.append(fila_dict)
 
-        # Crear un DataFrame con los datos
         df = pd.DataFrame(datos_dict)
 
-        # Verificar que el DataFrame no esté vacío
         if df.empty:
             raise Exception("El DataFrame está vacío.")
 
-        # Crear un archivo Excel en memoria
         output = BytesIO()
         with pd.ExcelWriter(output, engine='openpyxl') as writer:
-            # Escribir el DataFrame en una hoja de Excel
             df.to_excel(writer, index=False, sheet_name=tabla.__tablename__.capitalize())
 
-        # Preparar el archivo para ser descargado
         output.seek(0)
         return output
     except Exception as e:
